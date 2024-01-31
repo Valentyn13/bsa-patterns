aca8a9705c37f7d86f668015c358e52a7234a787
Valentyn13 <valentyn13git@gmail.com>
2024-01-31 16:21:22 +0200

    refactor index.ts file

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,10 @@ import { Database } from './data/database';
 import { CardHandler } from './handlers/card.handler';
 import { ListHandler } from './handlers/list.handler';
 import { ReorderService } from './services/reorder.service';
+import { ConsoleObserver, FileObserver } from './observer/observer';
+import path = require('path');
+import { formatDate } from './helpers/date.helpers';
+import { ProxyReorderLogger } from './proxy/reorder-proxy-logger';
 
 const PORT = 3003;
 
@@ -18,15 +22,26 @@ const io = new Server(httpServer, {
 });
 
 const db = Database.Instance;
-const reorderService = new ReorderService();
+const reorderService = new ProxyReorderLogger(new ReorderService());
 
 if (process.env.NODE_ENV !== "production") {
   db.setData(lists);
 }
+const date = formatDate(new Date().toISOString())
 
+const logPath = path.join(__dirname,'logs',`${date}.txt`)
+
+const listHandler = new ListHandler(io, db, reorderService)
+const cardHandler = new CardHandler(io, db, reorderService)
+
+const consoleObserver = new ConsoleObserver()
+const fileObserver = new FileObserver(logPath)
+
+cardHandler.subscribe(consoleObserver)
+cardHandler.subscribe(fileObserver)
 const onConnection = (socket: Socket): void => {
-  new ListHandler(io, db, reorderService).handleConnection(socket);
-  new CardHandler(io, db, reorderService).handleConnection(socket);
+  listHandler.handleConnection(socket);
+  cardHandler.handleConnection(socket);
 };
 
 io.on("connection", onConnection);
